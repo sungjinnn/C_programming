f6b03f053b83ebd461e5f4bf27beb2bfc39c3699
sungjinnn <127401478+sungjinnn@users.noreply.github.com>
2023-09-27 15:44:48 +0900

    hw2

--- hw2.c
+++ hw2.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 int main(void)
 {
 	double km, miles;
 	printf("Please enter kilometers: ");
 	scanf_s("%lf", &km);
-	miles = km * 0.62150403977;// 1/1.609=0.62150403977
+	miles = km * 0.62137
 	printf("%.1lf km is equal to %.1lf miles.\n", km, miles);
-
 	return 0;
 }
