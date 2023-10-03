32f757c5900fbde4079ddde292020d58c4647595
sungjinnn <127401478+sungjinnn@users.noreply.github.com>
2023-10-03 17:35:18 +0900

    Add files via upload

---
+++ hw3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int a, b, c;
+	for (a = 0; a < 5; a++) {
+		for (b = 5 - 1; b > a; b--)
+			printf(" ");
+
+		for (c = 0; c < 2 * a + 1; c++) {
+			printf("*");
+
+		}
+		printf("\n");
+	}
+	return 0;
+
+}
